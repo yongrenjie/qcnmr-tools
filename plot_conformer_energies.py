d7a045c102a1a11ca9c2d91d79e007ba7cbe22f8
Jonathan Yong <yongrenjie@gmail.com>
2019-02-13 18:47:52 +0000

    add plot_conformer_energies.py

---
+++ plot_conformer_energies.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filenames", action='store', help='.csv file(s) to plot energies from', nargs="*")
+    return parser.parse_args()
+
+def get_full_list(file):
+    conformer_numbers = []
+    conformer_energies = []
+    with open(file, 'r') as csv_file:
+        for line in csv_file:
+            if not line.lstrip().startswith(","):
+                conformer_numbers.append(int(line.rstrip("\n").split(",")[1]))
+                conformer_energies.append(float(line.rstrip("\n").split(",")[-1]))
+    return conformer_numbers, conformer_energies
+
+def get_short_list(file):
+    conformer_energies = []
+    with open(file, 'r') as csv_file:
+        for line in csv_file:
+            if not line.lstrip().startswith(","):
+                conformer_energies.append(float(line.rstrip("\n").split(",")[-1]))
+    return conformer_energies
+
+if __name__ == '__main__':
+    args = get_args()
+    file_number = 1
+    full_df = pd.DataFrame()
+    for filename in args.filenames:
+        base_name = filename.rstrip(".csv")
+        if file_number == 1:
+            num, ene = get_full_list(filename)
+            full_df.loc[:, 'num'] = num
+            full_df.loc[:, base_name] = ene
+        else:
+            ene = get_short_list(filename)
+            full_df.loc[:, base_name] = ene
+        file_number = file_number + 1
+    for column in full_df:
+        full_df[column] = full_df[column] - full_df[column][0]
+    print(full_df)
+
+    column_names = list(full_df.columns)
+    full_df.plot(x=0, y=column_names[1:], linewidth=0.75)
+    plt.show()
