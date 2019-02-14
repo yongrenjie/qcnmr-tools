d1945be275eab3acc628b9c4c543697a9f5c9766
Jonathan Yong <yongrenjie@gmail.com>
2019-02-14 23:07:12 +0000

    added functionality to parse optimisation output files

--- extract_energies.py
+++ extract_energies.py
@@ -18,22 +18,31 @@ def get_args():
 if __name__ == '__main__':
     args = get_args()
     threshold_kcal = args.threshold
-    sp_outputfile = args.filename
+    output_file = args.filename
 
     # determine what output file we are reading (CREST or ORCA)
     output_file_determined = False
+    orca_file_found = False
     output_file_type = ""
     while not output_file_determined:
-        with open(sp_outputfile, 'r') as outputfile:
+        with open(output_file, 'r') as outputfile:
             for line in outputfile:
                 if "C R E S T" in line:
                     output_file_type = "crest"
                     output_file_determined = True
                     break
                 elif "O   R   C   A" in line:
-                    output_file_type = "orca_sp"
-                    output_file_determined = True
-                    break
+                    orca_file_found = True
+                if orca_file_found:
+                    if "|  1> ! " in line:
+                        if "opt" in line.lower():
+                            output_file_type = "orca_opt"
+                            output_file_determined = True
+                            break
+                        else:
+                            output_file_type = "orca_sp"
+                            output_file_determined = True
+                            break
             else:
                 output_file_determined = True
                 sys.exit("Could not determine output file type.")
@@ -43,9 +52,9 @@ if __name__ == '__main__':
 
     if output_file_type == "crest":
         energy_block = False
-        with open(sp_outputfile, 'r') as outputfile:
+        with open(output_file, 'r') as outputfile:
             for line in outputfile:
-                if line.strip().startswith("number of unique conformers for further calc"):
+                if "number of unique conformers for further calc" in line:
                     energy_block = True
                 if line.strip().startswith("NMR mode"):
                     energy_block = False
@@ -56,11 +65,27 @@ if __name__ == '__main__':
         all_conformers = pd.DataFrame({'num': conformer_numbers, 'energy': conformer_energies})
 
     elif output_file_type == "orca_sp":
-        with open(sp_outputfile, 'r') as outputfile:
+        with open(output_file, 'r') as outputfile:
             for line in outputfile:
-                if line.strip().startswith("MULTIPLE XYZ STEP"):
+                if "MULTIPLE XYZ STEP" in line:
                     conformer_numbers.append(int(line.split()[-1]))
-                if line.strip().startswith("FINAL SINGLE POINT ENERGY"):
+                if "FINAL SINGLE POINT ENERGY" in line:
+                    conformer_energies.append(float(line.split()[-1]))
+
+        all_conformers = pd.DataFrame({'num': conformer_numbers, 'energy': conformer_energies})
+        all_conformers['energy'] = (all_conformers['energy'] - all_conformers['energy'].min()) * HARTREE_TO_KCAL
+
+    elif output_file_type == "orca_opt":
+        print("opt file found")
+        with open(output_file, 'r') as outputfile:
+            converged = False
+            for line in outputfile:
+                if "FINAL ENERGY EVALUATION AT THE STATIONARY POINT" in line:
+                    converged = True
+                if "MULTIPLE XYZ OPTIMIZATION STRUCTURE" in line:
+                    converged = False
+                    conformer_numbers.append(int(line.split()[-2]))
+                if converged and "FINAL SINGLE POINT ENERGY" in line:
                     conformer_energies.append(float(line.split()[-1]))
 
         all_conformers = pd.DataFrame({'num': conformer_numbers, 'energy': conformer_energies})
@@ -81,11 +106,11 @@ if __name__ == '__main__':
         filtered_conformers = filtered_conformers.reset_index(drop=True)
         print(filtered_conformers)
 
-        csv_filename = sp_outputfile.rstrip(".out") + "_filtered.csv"
+        csv_filename = output_file.rstrip(".out") + "_filtered.csv"
         filtered_conformers.to_csv(csv_filename, columns=['num','energy'])
         print("Conformers within {} kcal/mol written to {}.".format(threshold_kcal, csv_filename))
 
     if args.csv:
-        csv_filename = sp_outputfile.replace(".out", ".csv")
+        csv_filename = output_file.replace(".out", ".csv")
         all_conformers.to_csv(csv_filename, columns=['num','energy'])
         print("All conformers written to {}.".format(csv_filename))
