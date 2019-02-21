4753dc58aca4a222543c8afcc91677bf1339fffb
Jonathan Yong <yongrenjie@gmail.com>
2019-02-21 15:52:55 +0000

    now reads multiple optimisation outputs

--- extract_energies.py
+++ extract_energies.py
@@ -97,16 +97,45 @@ if __name__ == '__main__':
             all_conformers['energy'] = (all_conformers['energy'] - all_conformers['energy'].min()) * HARTREE_TO_KCAL
 
     else:
-        for file in args.filenames:
-            with open(file, 'r') as sp_output_file:
-                conformer_numbers.append(int(file.split(".")[-2].split("_")[-1])) # gets number from file name
-                for line in sp_output_file:
-                    if "FINAL SINGLE POINT ENERGY" in line:
-                        conformer_energies.append(float(line.split()[-1]))
-        all_conformers = pd.DataFrame({'num': conformer_numbers, 'energy': conformer_energies})
-        all_conformers['energy'] = (all_conformers['energy'] - all_conformers['energy'].min()) * HARTREE_TO_KCAL
-        all_conformers = all_conformers.sort_values('num')
-        all_conformers = all_conformers.reset_index(drop=True)
+        test_file = args.filenames[0]
+        with open(test_file, 'r') as determine_output_file:
+            for line in determine_output_file:
+                if "|  1> ! " in line:
+                    if "opt" in line.lower():
+                        output_file_type = "multiple_orca_opt"
+                        output_file_determined = True
+                        break
+                    else:
+                        output_file_type = "multiple_orca_sp"
+                        output_file_determined = True
+                        break
+
+        if output_file_type == "multiple_orca_opt":
+            for file in args.filenames:
+                with open(file, 'r') as opt_output_file:
+                    conformer_numbers.append(int(file.split(".")[-2].split("_")[-1]))  # gets number from file name
+                    converged = False
+                    for line in opt_output_file:
+                        if "FINAL ENERGY EVALUATION AT THE STATIONARY POINT" in line:
+                            converged = True
+                        if converged and "FINAL SINGLE POINT ENERGY" in line:
+                            conformer_energies.append(float(line.split()[-1]))
+                            break
+            all_conformers = pd.DataFrame({'num': conformer_numbers, 'energy': conformer_energies})
+            all_conformers['energy'] = (all_conformers['energy'] - all_conformers['energy'].min()) * HARTREE_TO_KCAL
+            all_conformers = all_conformers.sort_values('num')
+            all_conformers = all_conformers.reset_index(drop=True)
+        elif output_file_type == "multiple_orca_sp":
+            for file in args.filenames:
+                with open(file, 'r') as sp_output_file:
+                    conformer_numbers.append(int(file.split(".")[-2].split("_")[-1])) # gets number from file name
+                    for line in sp_output_file:
+                        if "FINAL SINGLE POINT ENERGY" in line:
+                            conformer_energies.append(float(line.split()[-1]))
+            all_conformers = pd.DataFrame({'num': conformer_numbers, 'energy': conformer_energies})
+            all_conformers['energy'] = (all_conformers['energy'] - all_conformers['energy'].min()) * HARTREE_TO_KCAL
+            all_conformers = all_conformers.sort_values('num')
+            all_conformers = all_conformers.reset_index(drop=True)
 
     print(all_conformers)
 
