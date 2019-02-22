58ef5c41faf9c75b4c4501e9fb48d03e66547499
Jonathan Yong <yongrenjie@gmail.com>
2019-02-22 17:09:25 +0000

    add analyse mode for couplings.py

--- couplings.py
+++ couplings.py
@@ -15,8 +15,6 @@ Then, it automatically averages the shifts and scales them according to the slop
 
 import argparse
 import pandas as pd
-import numpy as np
-
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -31,88 +29,75 @@ def get_args():
 
 def parse_coupling_file(file):
     with open(file, 'r') as data_file:
-        atom_1_labels = []
-        atom_2_labels = []
-        atom_1_types = []
-        atom_2_types = []
+        atom_labels = []
+        atom_types = []
         couplings = []
         population = 0
+        other_coupling = False
         for line in data_file:
             if "#  Population:" in line:
                 population = float(line.split()[-1])
             if "NUCLEUS A" in line and "NUCLEUS B" in line:
-                print(line)
-                atom_1_labels.append(line.split()[4])
-                atom_1_types.append(line.split()[3])
-                atom_2_labels.append(line.split()[-1])
-                atom_2_types.append(line.split()[-2])
-            if "Total" in line and "iso=" in line:
+                other_coupling = False
+                if line.split()[-2] == "O":
+                    other_coupling = True
+                else:
+                    atom_labels.append(line.split()[4] + "_" + line.split()[-1])
+                    atom_types.append(line.split()[3] + "_" + line.split()[-2])
+            if "Total" in line and "iso=" in line and not other_coupling:
                 couplings.append(float(line.split()[-1]))
-
-    return atom_1_labels, atom_1_types, atom_2_labels, atom_2_types, couplings, population
+    return atom_labels, atom_types, couplings, population
 
 
 if __name__ == '__main__':
     args = get_args()
-    '''
+    
     if args.analyse:
         file_number = 0
         for file in args.filenames:
             file_number = file_number + 1
             conformer_number = int(file.split(".")[-2].split("_")[1])  # gets conformer number from file name
             if file_number == 1:
-                atom_labels, atom_types, shieldings, population = parse_shielding_file(file)
+                atom_labels, atom_types, couplings, population = parse_coupling_file(file)
                 if atom_labels:
                     nmr_df = pd.DataFrame({
                         'atom_label': atom_labels,
                         'atom_type': atom_types,
-                        'conf_{}'.format(conformer_number): shieldings
+                        'conf_{}'.format(conformer_number): couplings
                     })
                     pop = pd.Series(population, index=['conf_{}'.format(conformer_number)])
             else:
-                atom_labels, atom_types, shieldings, population = parse_shielding_file(file)
+                atom_labels, atom_types, couplings, population = parse_coupling_file(file)
                 if atom_labels:
-                    nmr_df['conf_{}'.format(conformer_number)] = shieldings
+                    nmr_df['conf_{}'.format(conformer_number)] = couplings
                     pop['conf_{}'.format(conformer_number)] = population
         pop.name = "pop"
         full_df = nmr_df.append(pop)
 
         # average the shieldings according to population
-        full_df['avg_shield'] = 0.0
+        full_df['avg_coupl'] = 0.0
         total_pop = 0
         for name in full_df.columns:
             if name.startswith("conf"):
-                full_df['avg_shield'] = full_df['avg_shield'] + full_df[name]*full_df.at['pop',name]
+                full_df['avg_coupl'] = full_df['avg_coupl'] + full_df[name]*full_df.at['pop',name]
                 total_pop = total_pop + full_df.at['pop',name]
-        full_df.at['pop', 'avg_shield'] = total_pop
+        full_df.at['pop', 'avg_coupl'] = total_pop
 
-        # scale the shieldings to obtain chemical shifts
-        full_df['shift'] = 0.0
-        for i in full_df.index:
-            if isinstance(i, int):
-                if full_df.at[i, 'atom_type'] == "C":
-                    full_df.at[i, 'shift'] = (full_df.at[i, 'avg_shield'] - INTERCEPT_13C_PBE0_ccPVTZ)/SLOPE_13C_PBE0_ccPVTZ
-                elif full_df.at[i, 'atom_type'] == "H":
-                    full_df.at[i, 'shift'] = (full_df.at[i, 'avg_shield'] - INTERCEPT_1H_PBE0_ccPVTZ)/SLOPE_1H_PBE0_ccPVTZ
-        full_df.at['pop','shift'] = np.nan
         print(full_df)
 
-        csv_filename = "chemical_shift_data.csv"
+        csv_filename = "coupling_constant_data.csv"
         full_df.to_csv(csv_filename)
         print("Data written to {}.".format(csv_filename))
-    '''
-    if 1:
-    # else:
+
+    else:
         for file in args.filenames:
-            atom_1_labels, atom_1_types, atom_2_labels, atom_2_types, couplings, population = parse_coupling_file(file)
-            if atom_1_labels:
+            atom_labels, atom_types, couplings, population = parse_coupling_file(file)
+            if atom_labels:
                 print("FILE: {}".format(file))
                 print("pop: {}".format(population))
                 nmr_df = pd.DataFrame({
-                    'atom_1_label': atom_1_labels,
-                    'atom_1_type': atom_1_types,
-                    'atom_2_label': atom_2_labels,
-                    'atom_2_type': atom_2_types,
+                    'atom_labels': atom_labels,
+                    'atom_types': atom_types,
                     'coupling': couplings,
                 })
                 print(nmr_df)
