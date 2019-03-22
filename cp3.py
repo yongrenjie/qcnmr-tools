91b168d6c8daded0431d87548882ed6188ac140b
Jonathan Yong <yongrenjie@gmail.com>
2019-03-22 21:38:30 +0000

    Added full support for 13C/1H based CP3 calculations

--- cp3.py
+++ cp3.py
@@ -3,18 +3,10 @@
 import pandas as pd
 import math
 
-COMBINED_CORRECT_MEAN = 0.512
-COMBINED_CORRECT_SD = 0.209
-COMBINED_INCORRECT_MEAN = -0.637
-COMBINED_INCORRECT_SD = 0.499
-C13_CORRECT_MEAN = 0.547
-C13_CORRECT_SD = 0.253
-C13_INCORRECT_MEAN = -0.487
-C13_INCORRECT_SD = 0.533
-H1_CORRECT_MEAN = 0.478
-H1_CORRECT_SD = 0.305
-H1_INCORRECT_MEAN = -0.786
-H1_INCORRECT_SD = 0.835
+CORRECT_MEAN = [0.512, 0.547, 0.478]            # [Combined, 13C, 1H]
+CORRECT_SD = [0.209, 0.253, 0.305]
+INCORRECT_MEAN = [-0.637, -0.487, -0.786]
+INCORRECT_SD = [0.499, 0.533, 0.835]
 
 
 def normal_cdf(x, mean, sd):
@@ -28,33 +20,52 @@ def calculate_cp3_value(calc_a, calc_b, expt_a, expt_b):
     Dc_divide_De = pd.Series(Delta_c / Delta_e > 1)     # logical array
     sum_f3 = ((((Delta_e ** 3) / Delta_c) * Dc_divide_De) + ((Delta_e * Delta_c) * (1 - Dc_divide_De))).sum()
     sum_DeSquared = (Delta_e ** 2).sum()
-
     cp3 = sum_f3 / sum_DeSquared
+
     return cp3
 
+
 def cp3(atoms, calc_a, calc_b, expt_a, expt_b):
+    cp3_A = [0, 0, 0]
+    cp3_B = [0, 0, 0]
+    p_cp3_given_A = [0, 0, 0]
+    p_cp3_given_B = [0, 0, 0]
+    p_A_given_cp3 = [0, 0, 0]
+    p_B_given_cp3 = [0, 0, 0]
+
+    c13_logical_array = pd.Series(atoms == 'C')
+    h1_logical_array = pd.Series(atoms == 'H')
+
+    # calculate CP3 for assignment A (first calc column = first expt column)
+    cp3_A[0] = calculate_cp3_value(calc_a, calc_b, expt_a, expt_b)
+    cp3_A[1] = calculate_cp3_value(calc_a * c13_logical_array, calc_b * c13_logical_array,
+                                   expt_a * c13_logical_array, expt_b * c13_logical_array)
+    cp3_A[2] = calculate_cp3_value(calc_a * h1_logical_array, calc_b * h1_logical_array,
+                                   expt_a * h1_logical_array, expt_b * h1_logical_array)
+    # calculate CP3 for assignment B (first calc column = second expt column)
+    cp3_B[0] = calculate_cp3_value(calc_a, calc_b, expt_b, expt_a)
+    cp3_B[1] = calculate_cp3_value(calc_a * c13_logical_array, calc_b * c13_logical_array,
+                                   expt_b * c13_logical_array, expt_a * c13_logical_array)
+    cp3_B[2] = calculate_cp3_value(calc_a * h1_logical_array, calc_b * h1_logical_array,
+                                   expt_b * h1_logical_array, expt_a * h1_logical_array)
+
     print()
     print("==================================================")
     print()
-    # calculate CP3 for assignment A (first calc column = first expt column)
-    cp3_A = calculate_cp3_value(calc_a, calc_b, expt_a, expt_b)
-    # calculate CP3 for assignment B (first calc column = second expt column)
-    cp3_B = calculate_cp3_value(calc_a, calc_b, expt_b, expt_a)
-    # calculate probability of obtaining CP3 values given that assignment A is correct
-    p_cp3_given_A = min(normal_cdf(cp3_A, COMBINED_CORRECT_MEAN, COMBINED_CORRECT_SD),
-                        1 - normal_cdf(cp3_A, COMBINED_CORRECT_MEAN, COMBINED_CORRECT_SD)) * \
-                    min(normal_cdf(cp3_B, COMBINED_INCORRECT_MEAN, COMBINED_INCORRECT_SD),
-                        1 - normal_cdf(cp3_B, COMBINED_INCORRECT_MEAN, COMBINED_INCORRECT_SD))
-    # calculate probability of obtaining CP3 values given that assignment B is correct
-    p_cp3_given_B = min(normal_cdf(cp3_A, COMBINED_INCORRECT_MEAN, COMBINED_INCORRECT_SD),
-                        1 - normal_cdf(cp3_A, COMBINED_INCORRECT_MEAN, COMBINED_INCORRECT_SD)) * \
-                    min(normal_cdf(cp3_B, COMBINED_CORRECT_MEAN, COMBINED_CORRECT_SD),
-                        1 - normal_cdf(cp3_B, COMBINED_CORRECT_MEAN, COMBINED_CORRECT_SD))
-    # calculate probability of assignments A or B being correct given the CP3 values
-    p_A_given_cp3 = p_cp3_given_A / (p_cp3_given_A + p_cp3_given_B)
-    p_B_given_cp3 = p_cp3_given_B / (p_cp3_given_A + p_cp3_given_B)
-
-    #TODO: Add 13C and 1H analysis
+    for i in range(3): # i = 0: combined; i = 1: 13C; i = 2: 1H
+        # calculate probability of obtaining CP3 values given that assignment A is correct
+        p_cp3_given_A[i] = min(normal_cdf(cp3_A[i], CORRECT_MEAN[i], CORRECT_SD[i]),
+                               1 - normal_cdf(cp3_A[i], CORRECT_MEAN[i], CORRECT_SD[i])) * \
+                           min(normal_cdf(cp3_B[i], INCORRECT_MEAN[i], INCORRECT_SD[i]),
+                               1 - normal_cdf(cp3_B[i], INCORRECT_MEAN[i], INCORRECT_SD[i])) * 4
+        # calculate probability of obtaining CP3 values given that assignment B is correct
+        p_cp3_given_B[i] = min(normal_cdf(cp3_A[i], INCORRECT_MEAN[i], INCORRECT_SD[i]),
+                               1 - normal_cdf(cp3_A[i], INCORRECT_MEAN[i], INCORRECT_SD[i])) * \
+                           min(normal_cdf(cp3_B[i], CORRECT_MEAN[i], CORRECT_SD[i]),
+                               1 - normal_cdf(cp3_B[i], CORRECT_MEAN[i], CORRECT_SD[i])) * 4
+        # calculate probability of assignments A or B being correct given the CP3 values
+        p_A_given_cp3[i] = p_cp3_given_A[i] / (p_cp3_given_A[i] + p_cp3_given_B[i])
+        p_B_given_cp3[i] = p_cp3_given_B[i] / (p_cp3_given_A[i] + p_cp3_given_B[i])
 
     # print formatted output
     print("ASSIGNMENT A: ({}, {}) -> ({}, {})".format(expt_a.name, expt_b.name, calc_a.name, calc_b.name))
@@ -64,13 +75,26 @@ def cp3(atoms, calc_a, calc_b, expt_a, expt_b):
     print("-------------------      -------------------      -------------------")
     print("Combined 13C and 1H      13C only                 1H only")
     print("-------------------      -------------------      -------------------")
-    print("CP3(A)  : {:9.6f}      CP3(A)  : {:9.6f}      CP3(A)  : {:9.6f}".format(cp3_A, cp3_A, cp3_A))
-    print("P(CP3|A): {:9.6f}      P(CP3|A): {:9.6f}      P(CP3|A): {:9.6f}".format(p_cp3_given_A, p_cp3_given_A, p_cp3_given_A))
-    print("CP3(B)  : {:9.6f}      CP3(B)  : {:9.6f}      CP3(B)  : {:9.6f}".format(cp3_B, cp3_B, cp3_B))
-    print("P(CP3|B): {:9.6f}      P(CP3|B): {:9.6f}      P(CP3|B): {:9.6f}".format(p_cp3_given_B, p_cp3_given_B, p_cp3_given_B))
+    print("CP3(A)  : {:9.6f}      CP3(A)  : {:9.6f}      CP3(A)  : {:9.6f}".format(cp3_A[0],
+                                                                                   cp3_A[1],
+                                                                                   cp3_A[2]))
+    print("CP3(B)  : {:9.6f}      CP3(B)  : {:9.6f}      CP3(B)  : {:9.6f}".format(cp3_B[0],
+                                                                                   cp3_B[1],
+                                                                                   cp3_B[2]))
     print()
-    print("P(A|CP3): {:9.6f}      P(A|CP3): {:9.6f}      P(A|CP3): {:9.6f}".format(p_A_given_cp3, p_A_given_cp3, p_A_given_cp3))
-    print("P(B|CP3): {:9.6f}      P(B|CP3): {:9.6f}      P(B|CP3): {:9.6f}".format(p_B_given_cp3, p_B_given_cp3, p_B_given_cp3))
+    print("P(CP3|A): {:9.6f}      P(CP3|A): {:9.6f}      P(CP3|A): {:9.6f}".format(p_cp3_given_A[0],
+                                                                                   p_cp3_given_A[1],
+                                                                                   p_cp3_given_A[2]))
+    print("P(CP3|B): {:9.6f}      P(CP3|B): {:9.6f}      P(CP3|B): {:9.6f}".format(p_cp3_given_B[0],
+                                                                                   p_cp3_given_B[1],
+                                                                                   p_cp3_given_B[2]))
+    print()
+    print("P(A|CP3): {:9.6f}      P(A|CP3): {:9.6f}      P(A|CP3): {:9.6f}".format(p_A_given_cp3[0],
+                                                                                   p_A_given_cp3[1],
+                                                                                   p_A_given_cp3[2]))
+    print("P(B|CP3): {:9.6f}      P(B|CP3): {:9.6f}      P(B|CP3): {:9.6f}".format(p_B_given_cp3[0],
+                                                                                   p_B_given_cp3[1],
+                                                                                   p_B_given_cp3[2]))
 
     return 0
 
@@ -109,13 +133,15 @@ if __name__ == '__main__':
 
     number_of_isomers = len(calc_df.columns) - 2   # 2
 
-    for i in range(number_of_isomers): # 0, 1
-        for j in range(number_of_isomers):
-            if j > i:
+    for m in range(number_of_isomers):      # 0, 1
+        for n in range(number_of_isomers):
+            if n > m:
                 # Runs CP3 analysis for all pairs of columns in calculated/experimental data
-                cp3(calc_df['atom'], calc_df.iloc[:, i + 2], calc_df.iloc[:, j + 2],
-                    expt_df.iloc[:, i + 2], expt_df.iloc[:, j + 2])
+                cp3(calc_df['atom'], calc_df.iloc[:, m + 2], calc_df.iloc[:, n + 2],
+                    expt_df.iloc[:, m + 2], expt_df.iloc[:, n + 2])
 
+    print()
+    print("==================================================")
     print()
     print("Please cite: Smith, S. G.; Goodman, J. M. J. Org. Chem. 2009, 74 (12), 4597–4607.")
 
