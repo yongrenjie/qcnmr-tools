90bd30aae2b6fbaa4e0c7226f94717fa242d3e8b
Jonathan Yong <yongrenjie@gmail.com>
2019-03-22 17:33:31 +0000

    v0.2: actually calculates probabilities of assignments

--- cp3.py
+++ cp3.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
 
-import sys
 import pandas as pd
+import math
+
+COMBINED_CORRECT_MEAN = 0.512
+COMBINED_CORRECT_SD = 0.209
+COMBINED_INCORRECT_MEAN = -0.637
+COMBINED_INCORRECT_SD = 0.499
+C13_CORRECT_MEAN = 0.547
+C13_CORRECT_SD = 0.253
+C13_INCORRECT_MEAN = -0.487
+C13_INCORRECT_SD = 0.533
+H1_CORRECT_MEAN = 0.478
+H1_CORRECT_SD = 0.305
+H1_INCORRECT_MEAN = -0.786
+H1_INCORRECT_SD = 0.835
 
 
-def cp3(atoms, calc_a, calc_b, expt_a, expt_b):
-    print("ASSIGNMENT: ({}, {}) -> ({}, {})".format(expt_a.name, expt_b.name, calc_a.name, calc_b.name))
+def normal_cdf(x, mean, sd):
+    return (1.0 + math.erf((x - mean)/(sd * math.sqrt(2))))/2
+
+
+def calculate_cp3_value(calc_a, calc_b, expt_a, expt_b):
     Delta_c = calc_a - calc_b
     Delta_e = expt_a - expt_b
 
@@ -13,10 +29,32 @@ def cp3(atoms, calc_a, calc_b, expt_a, expt_b):
     sum_f3 = ((((Delta_e ** 3) / Delta_c) * Dc_divide_De) + ((Delta_e * Delta_c) * (1 - Dc_divide_De))).sum()
     sum_DeSquared = (Delta_e ** 2).sum()
 
-    cp3_combined = sum_f3 / sum_DeSquared
-    print("Combined CP3: {}".format(cp3_combined))
+    cp3 = sum_f3 / sum_DeSquared
+    return cp3
 
-    return cp3_combined
+def cp3(atoms, calc_a, calc_b, expt_a, expt_b):
+    print()
+    print("==================================================")
+    print("ASSIGNMENT: ({}, {}) -> ({}, {})".format(expt_a.name, expt_b.name, calc_a.name, calc_b.name))
+    print()
+    # calculate CP3 for assignment A (first calc column = first expt column)
+    cp3_A = calculate_cp3_value(calc_a, calc_b, expt_a, expt_b)
+    # calculate CP3 for assignment B (first calc column = second expt column)
+    cp3_B = calculate_cp3_value(calc_a, calc_b, expt_b, expt_a)
+    # calculate probability of obtaining CP3 values given that assignment A is correct
+    p_cp3_given_A = min(normal_cdf(cp3_A, COMBINED_CORRECT_MEAN, COMBINED_CORRECT_SD),
+                        1 - normal_cdf(cp3_A, COMBINED_CORRECT_MEAN, COMBINED_CORRECT_SD)) * \
+                    min(normal_cdf(cp3_B, COMBINED_INCORRECT_MEAN, COMBINED_INCORRECT_SD),
+                        1 - normal_cdf(cp3_B, COMBINED_INCORRECT_MEAN, COMBINED_INCORRECT_SD))
+    # calculate probability of obtaining CP3 values given that assignment B is correct
+    p_cp3_given_B = min(normal_cdf(cp3_A, COMBINED_INCORRECT_MEAN, COMBINED_INCORRECT_SD),
+                        1 - normal_cdf(cp3_A, COMBINED_INCORRECT_MEAN, COMBINED_INCORRECT_SD)) * \
+                    min(normal_cdf(cp3_B, COMBINED_CORRECT_MEAN, COMBINED_CORRECT_SD),
+                        1 - normal_cdf(cp3_B, COMBINED_CORRECT_MEAN, COMBINED_CORRECT_SD))
+    # calculate probability of assignments A or B being correct given the CP3 values
+    p_A_given_CP3 = p_cp3_given_A / (p_cp3_given_A + p_cp3_given_B)
+    p_B_given_CP3 = p_cp3_given_B / (p_cp3_given_A + p_cp3_given_B)
+    return [p_A_given_CP3, p_B_given_CP3]
 
 
 def parse_data(filename):
@@ -59,14 +97,7 @@ if __name__ == '__main__':
     for i in range(number_of_isomers): # 0, 1
         for j in range(number_of_isomers):
             if j > i:
-                print()
-                print("==================================================")
-                # calculate CP3 for the first way round ("correct")
-                cp3_correct = cp3(calc_df['atom'], calc_df.iloc[:, i+2], calc_df.iloc[:, j+2],
-                                  expt_df.iloc[:, i+2], expt_df.iloc[:, j+2])
-                # calculate CP3 for the other way round ("incorrect")
-                cp3_incorrect = cp3(calc_df['atom'], calc_df.iloc[:, i+2], calc_df.iloc[:, j+2],
-                                    expt_df.iloc[:, j+2], expt_df.iloc[:, i+2])
-
+                print(cp3(calc_df['atom'], calc_df.iloc[:, i + 2], calc_df.iloc[:, j + 2],
+                    expt_df.iloc[:, i + 2], expt_df.iloc[:, j + 2]))
 
 
