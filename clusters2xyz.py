a540897b0e4c70b0e269c33da3549016d60329fd
Jonathan Yong <yongrenjie@gmail.com>
2019-05-01 11:21:59 +0100

    merge changes

--- clusters2xyz.py
+++ clusters2xyz.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 import argparse
+
 
 def get_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--ligname", help="Residue name of ligand as used in GROMACS")
     parser.add_argument("-f", "--filename", help='File containing clustered conformers (default clusters.pdb)',
                         default="clusters.pdb")
     parser.add_argument("--minsize", help="Minimum size of cluster (i.e. number of conformations in cluster)"
@@ -38,19 +41,23 @@ def add_zeroes(j):
 
 if __name__ == "__main__":
     args = get_args()
-    
+
+    if not args.ligname:
+        print("Please provide the ligand name!!")
+        sys.exit()
+
     # could implement some smart searching here
     clust_file = args.filename
-    
+
     # could implement some smart searching here too
     log_file = "cluster.log"
     clusters_to_print = count_clusters(log_file, args.minsize)
-    
+
     # updates selection algebra for PyMol if explicit solvent is needed/desired
     selection_algebra = ""
     if args.explicit:
-        selection_algebra = " and byres all within 3.5 of resn jl1"
-    
+        selection_algebra = " and byres all within 3.5 of resn {}".format(args.ligname)
+
     # creates PyMol script which actually does the tough work
     with open("pymol.pml", "w") as pymol_s:
         print("load {}, clust".format(clust_file), file=pymol_s)
@@ -59,7 +66,7 @@ if __name__ == "__main__":
             if i >= args.restart:
                 print("save conf_{}.xyz, clust_{}{}".format(i, add_zeroes(i), selection_algebra), file=pymol_s)
         print("quit", file=pymol_s)
-    
+
     # runs PyMol script from command line and cleans up the folder
     os.system("pymol -c pymol.pml")
     os.system("rm pymol.pml")
